cca766040908357971ff52aba7351f934dd0a36f
Disha maurya <dishamaurya081@gmail.com>
2026-09-21 11:41:07 +0530

    lab3

--- lab3/prg2.cpp
+++ lab3/prg2.cpp
@@ -2,5 +2,28 @@
 
 using namespace std;
 
+class Time{
+    int hh,mm,ss;
+    public:
+     void input(int h=0,int m=0,int s=0);
+     void show();
 
+};
+void Time::input(int h,int m,int s){
+    hh=h;
+    mm=m;
+    ss=s;
+}
+void Time::show(){
+    cout<<hh<<":"<<mm<<":"<<ss<<endl;
+}
+int main(){
+    Time t1,t2;
+    int h,m,s;
+    cin>>h>>m>>s;
+    t1.input(h,m,s);
+    cin>>h>>m>>s;
+    t2.input(h,m,s);
+    t1.show();
+    t2.show();
 }
